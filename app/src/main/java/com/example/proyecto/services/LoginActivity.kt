83046c50cc1f0dab33fd356b241a2e940ef6b11e
Agustin Mamani <agusdmu1999@gmail.com>
2023-06-14 13:42:22 -0500

    agregando campos a la vista de registro

--- app/src/main/java/com/example/proyecto/services/LoginActivity.kt
+++ app/src/main/java/com/example/proyecto/services/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.example.proyecto.services
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.proyecto.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnRegister = findViewById<Button>(R.id.btnRegistro)
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/proyecto/services/RegisterActivity.kt
+++ app/src/main/java/com/example/proyecto/services/RegisterActivity.kt
@@ -1,12 +1,43 @@
 package com.example.proyecto.services
 
+import android.app.DatePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.EditText
 import com.example.proyecto.R
+import java.util.Calendar
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var btnPickDate: EditText
+    private lateinit var datePickerDialog: DatePickerDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        btnPickDate = findViewById(R.id.editTextDate)
+
+        btnPickDate.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+
+            datePickerDialog = DatePickerDialog(this,
+                DatePickerDialog.OnDateSetListener { view: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+                    val selectedDate = "$dayOfMonth/${month + 1}/$year"
+                    btnPickDate.setText(selectedDate)
+                }, year, month, dayOfMonth)
+
+            datePickerDialog.show()
+        }
+        val btnCancel = findViewById<Button>(R.id.btnCancel)
+        btnCancel.setOnClickListener {
+            finish()
+        }
     }
 }
