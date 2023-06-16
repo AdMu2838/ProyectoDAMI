911997c3018c1c4b5a2e32db1e41f61462fde140
Agustin Mamani <agusdmu1999@gmail.com>
2023-06-16 09:48:17 -0500

    validando registro

--- app/src/main/java/com/example/proyecto/services/LoginActivity.kt
+++ app/src/main/java/com/example/proyecto/services/LoginActivity.kt
@@ -1,20 +1,65 @@
 package com.example.proyecto.services
 
 import android.content.Intent
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.proyecto.R
+import com.example.proyecto.config.DatabaseHelper
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var databaseHelper: DatabaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        databaseHelper = DatabaseHelper(this)
         val btnRegister = findViewById<Button>(R.id.btnRegistro)
         btnRegister.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener{loginUser()
+
+        }
     }
+    private fun loginUser() {
+        // Obtener los datos del formulario de inicio de sesión
+        val editTextEmail: EditText = findViewById(R.id.editTextEmail)
+        val editTextPassword: EditText = findViewById(R.id.editTextPassword)
+
+        val email = editTextEmail.text.toString()
+        val password = editTextPassword.text.toString()
+
+        // Buscar el usuario en la base de datos
+        val db = databaseHelper.readableDatabase
+        val columns = arrayOf(DatabaseHelper.COLUMN_ID)
+        val selection = "${DatabaseHelper.COLUMN_EMAIL} = ? AND ${DatabaseHelper.COLUMN_PASSWORD} = ?"
+        val selectionArgs = arrayOf(email, password)
+        val cursor: Cursor? = db.query(
+            DatabaseHelper.TABLE_NAME,
+            columns,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            null
+        )
+
+        if (cursor != null && cursor.moveToFirst()) {
+            // El usuario existe y las credenciales son correctas
+            val userId = cursor.getLong(cursor.getColumnIndexOrThrow(DatabaseHelper.COLUMN_ID))
+            Toast.makeText(this, "Inicio de sesión exitoso. ID de usuario: $userId", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        } else {
+            // Las credenciales son incorrectas o el usuario no existe
+            Toast.makeText(this, "Credenciales incorrectas. Por favor, intenta nuevamente.", Toast.LENGTH_SHORT).show()
+        }
+        cursor?.close()
+    }
+
 }
--- app/src/main/java/com/example/proyecto/services/RegisterActivity.kt
+++ app/src/main/java/com/example/proyecto/services/RegisterActivity.kt
@@ -1,6 +1,7 @@
 package com.example.proyecto.services
 
 import android.app.DatePickerDialog
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,13 +9,15 @@ import android.view.View
 import android.widget.Button
 import android.widget.DatePicker
 import android.widget.EditText
+import android.widget.Toast
 import com.example.proyecto.R
+import com.example.proyecto.config.DatabaseHelper
 import java.util.Calendar
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var btnPickDate: EditText
     private lateinit var datePickerDialog: DatePickerDialog
-
+    private lateinit var databaseHelper: DatabaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -38,6 +41,43 @@ class RegisterActivity : AppCompatActivity() {
         val btnCancel = findViewById<Button>(R.id.btnCancel)
         btnCancel.setOnClickListener {
             finish()
+        }
+        // Inicializar el DatabaseHelper
+        databaseHelper = DatabaseHelper(this)
+
+        // ...
+
+        val btnRegister: Button = findViewById(R.id.btnRegister)
+        btnRegister.setOnClickListener { registerUser() }
+    }
+    private fun registerUser() {
+        val editTextName: EditText = findViewById(R.id.editTextName)
+        val editTextEmail: EditText = findViewById(R.id.editTextEmail)
+        val editTextPassword: EditText = findViewById(R.id.editTextPassword)
+        val editTextDate: EditText = findViewById(R.id.editTextDate)
+        val editTextPhoneNumber: EditText = findViewById(R.id.editTextPhoneNumber)
+
+        val name = editTextName.text.toString()
+        val email = editTextEmail.text.toString()
+        val password = editTextPassword.text.toString()
+        val birthdate = editTextDate.text.toString()
+        val phoneNumber = editTextPhoneNumber.text.toString()
+
+        // Insertar los datos en la base de datos
+        val db = databaseHelper.writableDatabase
+        val values = ContentValues().apply {
+            put(DatabaseHelper.COLUMN_NAME, name)
+            put(DatabaseHelper.COLUMN_EMAIL, email)
+            put(DatabaseHelper.COLUMN_PASSWORD, password)
+            put(DatabaseHelper.COLUMN_BIRTHDATE, birthdate)
+            put(DatabaseHelper.COLUMN_PHONE, phoneNumber)
+        }
+        val newRowId = db?.insert(DatabaseHelper.TABLE_NAME, null, values)
+
+        if (newRowId != -1L) {
+            Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Error en el registro", Toast.LENGTH_SHORT).show()
         }
     }
 }
