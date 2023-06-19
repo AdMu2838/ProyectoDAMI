4425e2fff1be80b8631d5c73a7dd6c0c993ced88
Agustin Mamani <agusdmu1999@gmail.com>
2023-06-19 11:00:28 -0500

    arreglando la demora en cargar los datos del perfil

--- app/src/main/java/com/example/proyecto/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/proyecto/fragments/ProfileFragment.kt
@@ -1,12 +1,15 @@
 package com.example.proyecto.fragments
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.proyecto.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -20,33 +23,20 @@ class ProfileFragment : Fragment() {
     ): View? {
 
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
-        // Obtener el usuario actualmente autenticado
-        val currentUser = FirebaseAuth.getInstance().currentUser
-        val userId = currentUser?.uid
 
-        if (userId != null) {
-            val db = FirebaseFirestore.getInstance()
-            val userRef = db.collection("users").document(userId)
+        // Leer los datos del perfil desde SharedPreferences
+        val sharedPrefs = context?.getSharedPreferences("UserProfile", Context.MODE_PRIVATE)
+        val name = sharedPrefs?.getString("name", "")
+        val email = sharedPrefs?.getString("email", "")
+        val date = sharedPrefs?.getString("date", "")
+        val phoneNumber = sharedPrefs?.getString("phoneNumber", "")
 
-            userRef.get()
-                .addOnSuccessListener { document ->
-                    if (document.exists()) {
-                        val name = document.getString("name")
-                        val email = document.getString("email")
-                        val date = document.getString("date")
-                        val phoneNumber = document.getString("phoneNumber")
+        // Actualizar la interfaz de usuario con los datos del perfil
+        updateProfileUI(view, name, email, date, phoneNumber)
 
-                        // Actualiza la interfaz de usuario con los datos del perfil
-                        updateProfileUI(name, email, date, phoneNumber)
-                    }
-                }
-                .addOnFailureListener { exception ->
-                    // Ocurrió un error al obtener los datos del perfil
-                }
-        }
         return view
     }
-    private fun updateProfileUI(name: String?, email: String?, date: String?, phoneNumber: String?) {
+    private fun updateProfileUI(view: View?, name: String?, email: String?, date: String?, phoneNumber: String?) {
         val nameTextView = view?.findViewById<TextView>(R.id.nameTextView)
         val emailTextView = view?.findViewById<TextView>(R.id.emailTextView)
         val dateTextView = view?.findViewById<TextView>(R.id.dateTextView)
--- app/src/main/java/com/example/proyecto/services/MainActivity.kt
+++ app/src/main/java/com/example/proyecto/services/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.proyecto.services
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -7,13 +8,14 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.proyecto.R
+import com.example.proyecto.fragments.ProfileFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var firestore: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -26,6 +28,38 @@ class MainActivity : AppCompatActivity() {
 
         AppBarConfiguration(navController.graph)
         navView.setupWithNavController(navController)
+        // Obtener el usuario actualmente autenticado
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val userId = currentUser?.uid
+
+        if (userId != null) {
+            val db = FirebaseFirestore.getInstance()
+            val userRef = db.collection("users").document(userId)
+
+            userRef.get()
+                .addOnSuccessListener { document ->
+                    if (document.exists()) {
+                        val name = document.getString("name")
+                        val email = document.getString("email")
+                        val date = document.getString("date")
+                        val phoneNumber = document.getString("phoneNumber")
+
+                        // Guardar los datos del perfil en SharedPreferences
+                        val sharedPrefs = getSharedPreferences("UserProfile", Context.MODE_PRIVATE)
+                        val editor = sharedPrefs.edit()
+                        editor.putString("name", name)
+                        editor.putString("email", email)
+                        editor.putString("date", date)
+                        editor.putString("phoneNumber", phoneNumber)
+                        editor.apply()
+
+
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    // Ocurrió un error al obtener los datos del perfil
+                }
+        }
 
 
     }
