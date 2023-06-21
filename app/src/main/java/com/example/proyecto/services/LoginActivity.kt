fd637f1de9cc32f6d0d6319d182e2b8b04cfb456
Agustin Mamani <agusdmu1999@gmail.com>
2023-06-21 14:43:26 -0500

    OPCION DE INGRESO CON GOOGLE

--- app/src/main/java/com/example/proyecto/services/LoginActivity.kt
+++ app/src/main/java/com/example/proyecto/services/LoginActivity.kt
@@ -8,62 +8,124 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.example.proyecto.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 123
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        // Habilitar la persistencia de la sesión en Firebase Authentication
+        // Configurar el inicio de sesión con Google
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = FirebaseAuth.getInstance()
 
         val btnRegister = findViewById<Button>(R.id.btnRegistro)
         btnRegister.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+
         val btnLogin = findViewById<Button>(R.id.btnLogin)
-        btnLogin.setOnClickListener{loginUser()
+        btnLogin.setOnClickListener {
+            loginUser()
+        }
 
+        val btnGoogleSignIn = findViewById<SignInButton>(R.id.btnGoogleSignIn)
+        btnGoogleSignIn.setOnClickListener {
+            signInWithGoogle()
         }
     }
+
     private fun loginUser() {
-            val email = findViewById<EditText>(R.id.editTextEmail).text.toString().trim()
-            val password = findViewById<EditText>(R.id.editTextPassword).text.toString().trim()
+        val email = findViewById<EditText>(R.id.editTextEmail).text.toString().trim()
+        val password = findViewById<EditText>(R.id.editTextPassword).text.toString().trim()
 
-            // Validar los campos de entrada
-            if (TextUtils.isEmpty(email)) {
-                Toast.makeText(this, "Ingresa tu correo electrónico", Toast.LENGTH_SHORT).show()
-                return
-            }
+        // Validar los campos de entrada
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Ingresa tu correo electrónico", Toast.LENGTH_SHORT).show()
+            return
+        }
 
-            if (TextUtils.isEmpty(password)) {
-                Toast.makeText(this, "Ingresa tu contraseña", Toast.LENGTH_SHORT).show()
-                return
-            }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Ingresa tu contraseña", Toast.LENGTH_SHORT).show()
+            return
+        }
 
-            // Iniciar sesión con Firebase Authentication
-        val currentUser = FirebaseAuth.getInstance().currentUser
-        if (currentUser != null){
-            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        // El inicio de sesión fue exitoso, el usuario está autenticado
-                        Toast.makeText(this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show()
+        // Iniciar sesión con Firebase Authentication
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // El inicio de sesión fue exitoso, el usuario está autenticado
+                    Toast.makeText(this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show()
 
-                        val intent = Intent(this, MainActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    } else {
-                        // Ocurrió un error durante el inicio de sesión
-                        Toast.makeText(
-                            this,
-                            "Error al iniciar sesión: ${task.exception?.message}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // Ocurrió un error durante el inicio de sesión
+                    Toast.makeText(
+                        this,
+                        "Error al iniciar sesión: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
+            }
+    }
+
+    private fun signInWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account?.idToken)
+            } catch (e: ApiException) {
+                Toast.makeText(this, "Error al iniciar sesión con Google: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
+    private fun firebaseAuthWithGoogle(idToken: String?) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // El inicio de sesión con Google fue exitoso, el usuario está autenticado
+                    Toast.makeText(this, "Inicio de sesión con Google exitoso", Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // Ocurrió un error durante el inicio de sesión con Google
+                    Toast.makeText(
+                        this,
+                        "Error al iniciar sesión con Google: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
 }
